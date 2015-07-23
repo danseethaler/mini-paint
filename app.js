0cc577c198f7fc26f554c02e288a5b719e829376
Dan Seethaler <dan@seethalers.com>
2015-07-22 19:40:21 -0600

    All done.

--- app.js
+++ app.js
@@ -1,5 +1,38 @@
 $(document).ready(function(){
+
+    var color = white;
+     var colors = 'white green red blue yellow';
+
     $('.box').on('click', function(){
-        $(this).addClass("white");
+        $(this).addClass(color);
     });
+
+    $('.box').on('dblclick', function(){
+        $(this).removeClass(color);
+    });
+
+    $('#reset').on('click', function(){
+        $('.box').removeClass(colors);
+    });
+
+    $('#white').on('click', function(){
+        color = "white";
+    })
+
+    $('#red').on('click', function(){
+        color = "red";
+    })
+
+    $('#green').on('click', function(){
+        color = "green";
+    })
+
+    $('#yellow').on('click', function(){
+        color = "yellow";
+    })
+
+    $('#blue').on('click', function(){
+        color = "blue";
+    })
+
 })
